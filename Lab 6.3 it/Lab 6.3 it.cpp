581f1662f89680b6e789a4455296ee56ebfbc6ef
Volodya22211 <Volodymyr.Baran.RI.2023@lpnu.ua>
2023-11-05 22:03:05 +0200

    Add project files.

---
+++ Lab 6.3 it/Lab 6.3 it.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <random>
+using namespace std;
+
+
+void printArray(int* array, int size);
+void generateArray(int* arr, int size, int min, int max);
+int FindBiggestElement(const int* arr, int size, int BiggestNum);
+
+template <typename T>
+void printArray1(T* array, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << setw(4) << array[i] << " ";
+    }
+    cout << endl;
+}
+
+template <typename T>
+void generateArray1(T* arr, int size, int min, int max) {
+    srand(unsigned(time(NULL)));
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = min + rand() % (max - min + 1);
+    }
+}
+
+template <typename T>
+T FindBiggestElement1(const T* arr, int size, T BiggestNum) {
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] % 2 == 0 && arr[i] > BiggestNum) {
+            BiggestNum = arr[i];
+        }
+    }
+
+    return BiggestNum;
+}
+
+
+int main() {
+    const int arrSize = 10;
+    int a[arrSize];
+    
+    generateArray(a, arrSize, -100, 100);
+
+    cout << "Array Elements: ";
+    printArray(a, arrSize);
+    
+    int biggestNumber = FindBiggestElement(a, arrSize, INT_MIN);
+    cout << "Biggest Number in Array which divides 2: " << biggestNumber << endl;
+   
+    const int arrSize1 = 10;
+    int b[arrSize1];
+
+    generateArray1(b, arrSize1, -100, 100);
+    cout << "Array Elements: ";
+    printArray1(b, arrSize1);
+
+    int biggestNumber1 = FindBiggestElement1(b, arrSize1, INT_MIN);
+    cout << "Biggest Number in Array which divides 2: " << biggestNumber1;
+    return 0;
+}
+
+void printArray(int* array, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << setw(4) << array[i] << " ";
+    }
+    cout << endl;
+}
+
+void generateArray(int* arr, int size, int min, int max) {
+    srand(unsigned(time(NULL)));
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = min + rand() % (max - min + 1);
+    }
+}
+
+int FindBiggestElement(const int* arr, int size, int BiggestNum) {
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] % 2 == 0 && arr[i] > BiggestNum) {
+            BiggestNum = arr[i];
+        }
+    }
+
+    return BiggestNum;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 6.3 it/Lab 6.3 it.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a[10] = { -78, 2, 98, 100, 70, 80, 4, 56, 59, 61 };
+			int c = FindBiggestElement(a, 10, INT_MIN);
+		}
+	};
+}
